483875b8a5554fa56934e4cd267e8e1611d45f03
Grace <graciousdaze@gmail.com>
2020-02-26 13:46:32 -0600

    Add javadoc comments

--- src/AdventurerNode.java
+++ src/AdventurerNode.java
@@ -1,5 +1,6 @@
 /**
- * DESCRIPTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * Used as a part of a linked list that stores adventurer objects. Each node
+ * contains an adventurer object and a link to the next node in the list.
  * 
  * @author Grace O'Brien
  * March 5th, 2020
@@ -7,8 +8,8 @@
  */
 public class AdventurerNode {
 	
-	private Adventurer data;
-	private AdventurerNode link;
+	private Adventurer data;	//Adventurer being held
+	private AdventurerNode link;	//Link to the next node in list
 	
 	public AdventurerNode(Adventurer data, AdventurerNode link)
 	{
@@ -16,21 +17,41 @@ public class AdventurerNode {
 		this.link = link;
 	}
 	
+	/**
+	 * Updates the node's data
+	 * 
+	 * @param data Adventurer to replace the data
+	 */
 	public void setData(Adventurer data)
 	{
 		this.data = data;
 	}
 	
+	/**
+	 * Returns the node's data
+	 * 
+	 * @return The node's data, an Adventurer object
+	 */
 	public Adventurer getData()
 	{
 		return data;
 	}
 	
+	/**
+	 * Updates the node's link
+	 * 
+	 * @param link The AdventurerNode for link to be updated to
+	 */
 	public void setLink(AdventurerNode link)
 	{
 		this.link = link;
 	}
 	
+	/**
+	 * Return's the Node's link, which is the next node in the list
+	 * 
+	 * @return The AdventurerNode that is next in the list
+	 */
 	public AdventurerNode getLink()
 	{
 		return link;
